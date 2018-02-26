886fb146d839bcd889f187be1b5b52b5efe3abab
jazzz1994 <jazzz1994aps@gmail.com>
2018-02-26 21:27:56 +0530

    php refactoring

---
+++ common/scrool.php
@@ -0,0 +1,3 @@
+<?php echo'<a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-sm btn-inverse">
+  <i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
+</a>'; ?>
---
+++ php/index.php
@@ -0,0 +1,22 @@
+<?php
+include '../common/header.php';?>
+
+<?php
+include '../common/nav_bar.php';
+?>
+
+
+<?php
+include '../common/side_bar.php'
+?>
+
+<?php
+include '../common/copyright.php'
+?>
+
+<?php
+include '../common/scrool.php'
+?>
+<?php
+include '../common/footer.php';
+?>
